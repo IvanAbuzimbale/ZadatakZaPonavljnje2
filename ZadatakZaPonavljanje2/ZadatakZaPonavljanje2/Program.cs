40ee8a189e07dce4a4aa78af74620359007e29db
IvanAbuzimbale <58472137+IvanAbuzimbale@users.noreply.github.com>
2021-06-06 15:59:48 +0200

    Add files via upload

---
+++ ZadatakZaPonavljanje2/ZadatakZaPonavljanje2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZadatakZaPonavljanje2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int iznos;
+            Console.Write("Upišite vrijednost u sekundama: ");
+            iznos = Convert.ToInt32(Console.ReadLine());
+            TimeSpan t = TimeSpan.FromSeconds(iznos);
+            Console.WriteLine("Vaša vrijednost u sekundama je jednaka kao: ");
+            Console.Write(t.Minutes + " minuta i " + t.Seconds + " sekundi.");
+            Console.ReadKey();
+            
+        }
+    }
+}
